a30ae68ec329e86972c47adc42a782c32241f1d8
hedricsd <hedricsd@rose-hulman.edu>
2019-01-10 15:11:27 -0500

    Done

--- src/m1_robot_motion_with_sensors.py
+++ src/m1_robot_motion_with_sensors.py
@@ -156,7 +156,7 @@ class SimpleRoseBot(object):
                 break
         self.stop()
 
-    
+
 
 ###############################################################################
 # The  Motor   and   ColorSensor classes.  USE them, but do NOT modify them.
